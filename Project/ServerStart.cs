062fe9c02a90680db3ff111190473afd93f7a421
Benjamin Porsmose Pedersen <bpeder19@student.aau.dk>
2021-10-28 11:24:52 +0200

    ..

---
+++ Project/ServerStart.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class ServerStart
+    {
+        public void Start()
+        {
+            // Create a Http server and start listening for incoming connections
+            Server.listener = new HttpListener();
+            Server.listener.Prefixes.Add(Server.url);
+            Server.listener.Start();
+            Console.WriteLine("Listening for connections on {0}", Server.url);
+
+            // Handle requests
+            Task listenTask = Server.HandleIncomingConnections();
+            listenTask.GetAwaiter().GetResult();
+
+            // Close the listener
+            Server.listener.Close();
+        }
+    }
+}
