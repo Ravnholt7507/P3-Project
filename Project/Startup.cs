57a7c09322af76b01e3a6ceb84f95eb283d8e933
Smedten <miksmedt@gmail.com>
2021-11-04 12:16:38 +0100

    Nye filer til at filtrere produkter (virker ikke helt endnu)

--- Project/Startup.cs
+++ Project/Startup.cs
@@ -29,6 +29,7 @@ namespace Project
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<Product>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
