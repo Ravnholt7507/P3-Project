685dc49acb9290ad409fe3bc994e6b6e2d09a0ec
Benjamin Porsmose Pedersen <bpeder19@student.aau.dk>
2021-11-04 13:04:36 +0100

    DBStuff

---
+++ Project/Data/CustomType.cs
@@ -0,0 +1,13 @@
+namespace Project.Data
+{
+    public class CustomType
+    {
+        public CustomType(string id, string type)
+        {
+            this.Id = id;
+            this.Type = type;
+        }
+        public string Id { get; set; }
+        public string Type { get; set; }
+    }
+}
