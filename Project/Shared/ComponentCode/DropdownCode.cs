3d9a8880eb0d2101655960e75635a91490ec8f0a
Run396 <Run396@users.noreply.github.com>
2021-11-09 12:41:07 +0100

    Component Map

---
+++ Project/Shared/ComponentCode/DropdownCode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Shared.ComponentCode
+{
+    public class DropdownCode : ComponentBase
+    {
+        public string Visible;
+        private int i = 1;
+
+        public void Dropdownbtn()
+        {
+            i++;
+            if (i % 2 == 0)
+            {
+                Visible = "block";
+            }
+            else
+            {
+                Visible = "none";
+            }
+        }
+        public List<string> InsertPages = new List<string>() { "Counter", "Admin", "Jackets", "Andreas", "Products" };       
+    }
+}
