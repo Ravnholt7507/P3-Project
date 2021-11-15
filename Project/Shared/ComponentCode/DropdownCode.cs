65bc7fe4008687d8f3ad7549b2fb4d3058e9a550
Run396 <Run396@users.noreply.github.com>
2021-11-15 12:02:31 +0100

    hglnk;gm

--- Project/Shared/ComponentCode/DropdownCode.cs
+++ Project/Shared/ComponentCode/DropdownCode.cs
@@ -21,6 +21,6 @@ namespace Project.Shared.ComponentCode
             }
         }
 
-        public List<string> InsertPages = new List<string>() { "Counter", "Admin", "Jackets", "Andreas", "Products" };
+        public List<string> InsertPages = new List<string>() { "Admin", "Jackets", "Andreas", "Products" };
     }
 }
