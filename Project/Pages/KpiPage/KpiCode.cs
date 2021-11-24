bba765ee5704a65c7cc0633b5bed78f6670f24ea
Run396 <Run396@users.noreply.github.com>
2021-11-24 14:32:49 +0100

    KPI Start

---
+++ Project/Pages/KpiPage/KpiCode.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Pages.KpiPage 
+{
+    public class KpiCode : ComponentBase
+    {
+        public void Popup()
+        {
+
+        }
+    }
+}
