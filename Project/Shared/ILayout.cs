a547e1300fac726705583d91114e538e38650c3c
Run396 <Run396@users.noreply.github.com>
2021-12-13 11:52:31 +0100

    Admin routing fix, dropdown, and small fixes hist og pist

---
+++ Project/Shared/ILayout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.CSharpFiles;
+
+namespace Project.Shared
+{
+    interface ILayout
+    {
+        bool DataBaseVerify(string AccesToken);
+    }
+}
