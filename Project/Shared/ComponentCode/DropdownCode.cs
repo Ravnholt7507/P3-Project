93196ac43e4dc9bb1458f8f1129acb9ed69d9267
Ravnholt7507 <Andreas.ravnholt99@gmail.com>
2021-12-16 17:16:41 +0100

    navbar kinda fix

--- Project/Shared/ComponentCode/DropdownCode.cs
+++ Project/Shared/ComponentCode/DropdownCode.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 using Project.CSharpFiles;
+using System.Threading.Tasks;
 
 namespace Project.Shared.ComponentCode
 {
     public class DropdownCode : ComponentBase
     {
+        public string selectedCat;
         public string Visible;
         private int i = 1;
 
@@ -25,14 +27,20 @@ namespace Project.Shared.ComponentCode
         DbCall call = new DbCall();
         public List<string> Pages = new List<string>();
 
-        public void DbCallType()
+        DbCall _call = new DbCall();
+        public string[][] categoryArray;
+        protected override Task OnInitializedAsync()
         {
-            Pages.Clear();
-            var array = call.KPI("Type call");
-            foreach (string item in array)
-            {
-                Pages.Add(item);
-            }
+            categoryArray = _call.AdminPages("Get", "Categories");
+            System.Console.WriteLine(categoryArray[0][0]);
+            return base.OnInitializedAsync();
+        }
+        public string[][] typeArray;
+        public void CallSubcats(string selectedCat)
+        {
+            System.Console.WriteLine("asdfas" + selectedCat);
+            typeArray = _call.AdminPages("Get", "Types", selectedCat);
+            
         }
     }
 }
